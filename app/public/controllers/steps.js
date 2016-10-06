6926c416feda22a8939b25c14cdaa90682e238a2
Artem <kalini.art@gmail.com>
2016-10-06 16:12:26 +0300

    reg

--- app/public/controllers/steps.js
+++ app/public/controllers/steps.js
@@ -1,28 +1,2 @@
 
 
-app.controller('stepsController',['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-    $scope.getSteps = function () {
-        $http.get('/api/steps').success(function(response){
-            $scope.steps = response;
-        });
-    };
-    $scope.getStep = function () {
-        var id = $routeParams.id;
-        $http.get('/api/steps/'+id).success(function(response){
-            $scope.step = response;
-        });
-    };
-    $scope.addStep = function(){
-        $scope.step.create_date = Date.now();
-        $http.post('/api/steps/', $scope.step).success(function (response) {
-            window.location.href='#/';
-        });
-    };
-
-    $scope.updateStep = function(){
-        var id = $routeParams.id;
-        $http.put('/api/steps/'+id, $scope.step).success(function (response) {
-            window.location.href='#/steps/'+id;
-        });
-    };
-}]);
--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,27 +1,60 @@
-var app = angular.module('OneStep',['ngRoute']);
+var app = angular.module('OneStep',['ui.router']);
 
 
-app.config(function($routeProvider){
-    $routeProvider.when('/',{
-        controller: 'stepsController',
-        templateUrl: 'views/steps.html'
-    })
-    .when('/addstep',{
+app.config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home',{
+        url:"/",
+        templateUrl: 'views/steps.html',
+        controller: 'stepsController'
+    });
+    $stateProvider.state('addstep',{
+        url: '/addstep',
         controller: 'stepsController',
         templateUrl: 'views/addstep.html'
-    })
-    .when('/steps/:id',{
+    });
+    $stateProvider.state('stepsId',{
+        url: '/steps/:id',
         controller: 'stepsController',
         templateUrl: 'views/detailstep.html'
-    })
-    .when('/steps/edit/:id',{
+    });
+    $stateProvider.state('stepsEditId',{
+        url: '/steps/edit/:id',
         controller: 'stepsController',
         templateUrl: 'views/editstep.html'
-    })
-    .otherwise({
-        redirectTo: '/'
     });
 });
 
+
+app.controller('stepsController',['$scope', '$http', '$location', '$stateParams', function($scope, $http, $location, $stateParams){
+
+
+    $scope.getSteps = function () {
+        $http.get('/api/steps').success(function(response){
+            $scope.steps = response;
+        });
+    };
+    $scope.getStep = function () {
+        var id = $stateParams.id;
+        $http.get('/api/steps/'+id).success(function(response){
+            $scope.step = response;
+        });
+    };
+    $scope.addStep = function(){
+        $scope.step.create_date = Date.now();
+        $http.post('/api/steps/', $scope.step).success(function (response) {
+            window.location.href='#/';
+        });
+    };
+
+    $scope.updateStep = function(){
+        var id = $stateParams.id;
+        $http.put('/api/steps/'+id, $scope.step).success(function (response) {
+            window.location.href='#/steps/'+id;
+        });
+    };
+}]);
 
 
